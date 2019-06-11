06d51a3bd6c94197a918eeb78a219773d97de831
Justin D <justin@baal-software.com>
2019-06-11 16:41:00 -0600

    Adds tests for turn

--- test/flashcards/main_test.clj
+++ test/flashcards/main_test.clj
@@ -2,12 +2,20 @@
   (:require [clojure.test :refer :all]
             [flashcards.main :refer :all]))
 
-(deftest new-card-test
-  (testing "A new card can be created"
-    (let [actual (new-card "what is?" "nothing is" :facts)
-          expected {:question "what is?" :answer "nothing is" :category :facts}]
-      (is (=  actual expected))
-      (is (= (:question actual) (:question expected)))
-      (is (= (:answer actual) (:answer expected)))
-      (is (= (:category actual) (:category expected))))))
+(def card_1 (card "what is?" "nothing is" :facts))
+(def turn_1 (turn "nothing is" card_1))
 
+(deftest card-test
+  (testing "A new card can be created"
+    (let [expected {:question "what is?" :answer "nothing is" :category :facts}]
+      (is (=  card_1 expected))
+      (is (= (:question card_1) (:question expected)))
+      (is (= (:answer card_1) (:answer expected)))
+      (is (= (:category card_1) (:category expected))))))
+
+(deftest turn-test
+  (testing "A turn can hold a card and have some derived values"
+    (is (= (:card turn_1) card_1))
+    (is (= (:guess turn_1) "nothing is"))
+    (is (= (:correct? turn_1) true))
+    (is (= (:feedback turn_1) "Correct!"))))
