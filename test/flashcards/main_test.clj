ff9926fd42aefc46f68d41f36966bd4fe4d8a7a1
Justin D <justin@baal-software.com>
2019-06-11 16:24:17 -0600

    Adjusts test for new-card function

--- test/flashcards/main_test.clj
+++ test/flashcards/main_test.clj
@@ -4,5 +4,10 @@
 
 (deftest new-card-test
   (testing "A new card can be created"
-    (let [actual (new-card "what is?" "nothing is" :facts)]
-    (is (= {:question "what is?" :answer "nothing is" :category :facts} actual)))))
+    (let [actual (new-card "what is?" "nothing is" :facts)
+          expected {:question "what is?" :answer "nothing is" :category :facts}]
+      (is (=  actual expected))
+      (is (= (:question actual) (:question expected)))
+      (is (= (:answer actual) (:answer expected)))
+      (is (= (:category actual) (:category expected))))))
+
