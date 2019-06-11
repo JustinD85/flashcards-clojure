ff59f7d205c763e7849065c5d9344f133bc352f9
Justin D <justin@baal-software.com>
2019-06-11 17:01:43 -0600

    Adds test to check incorrect answer status of turn

--- src/flashcards/main.clj
+++ src/flashcards/main.clj
@@ -5,10 +5,11 @@
   {:question question :answer answer :category category})
 
 (defn turn [guess card]
+  (let [correct? (= guess (:answer card))]
   {:card card
    :guess guess
-   :correct? (= guess (:answer card))
-   :feedback "Correct!"})
+   :correct? correct?
+   :feedback (if correct? "Correct!" "Incorrect.")}))
 
 (defn -main
   "I don't do a whole lot ... yet."
