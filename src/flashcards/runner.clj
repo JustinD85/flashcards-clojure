e92aa124676efef34ba8a8643211b9cc18d9c10c
Justin D <justin@baal-software.com>
2019-06-15 09:21:43 -0600

    Adds clojure/string to namespace

--- src/flashcards/runner.clj
+++ src/flashcards/runner.clj
@@ -1,10 +1,19 @@
 (ns flashcards.runner
  (:gen-class)
- (:require [flashcards.main :refer :all]))
+ (:require [flashcards.main :refer :all]
+           [clojure.string :as s]))
 
+;;grabs desired data
+(def german-words-file "resources/german_english.csv")
+(def raw-data (slurp german-words-file))
 
+;;helper functions
+(defn parse-data [data] (map #(s/split % #",") (s/split data #"\n")))
+(def create-card (partial apply card))
 
-
+;;converts to vector of cards
+(def cards
+  (map create-card (parse-data raw-data)))
 
 
 
