d6a325f1c1e95109019f1e3f0af6e6458745fccf
Justin D <justin@baal-software.com>
2019-06-15 11:59:42 -0600

    Changes runner file to be friendly to read and adjust dataset

--- src/flashcards/runner.clj
+++ src/flashcards/runner.clj
@@ -4,8 +4,8 @@
            [clojure.string :as s]))
 
 ;;grabs desired data
-(def german-words-file "resources/german_english.csv")
-(def raw-data (slurp german-words-file))
+(def flashcard-data-file "resources/german_english.csv")
+(def raw-data (slurp flashcard-data-file))
 
 ;;helper functions
 (defn parse-data [data] (map #(s/split % #",") (s/split data #"\n")))
@@ -15,21 +15,16 @@
 (def get-input (comp s/trim read-line))
 (def take-turn (comp :feedback #(% (get-input)) #(:take-turn %)))
 (def categories (set (map #(last %) (parse-data raw-data))))
+(defn correct-by-category [trailing-chars function]
+  (apply str (map #(str "\n" (function %) trailing-chars %) categories)))
 
 ;;converts to vector of cards
 (def cards
   (map create-card (parse-data raw-data)))
 
-;; binds a deck
-(def german-english-deck
-  (deck cards))
-
-;;TODO multiple rounds
-;;TODO multiple decks
-
 ;;binds a round
 (def round-1
-  (round german-english-deck))
+  (round (deck cards)))
 
 (defn -main []
   (println "Welcome to FlashCards!")
@@ -40,16 +35,14 @@
     (println "This is card" (card-number round-1)  "out of" (count cards))
     (println "Question:" (get-question round-1))
     (println "Your answer is"(take-turn round-1))
-    (println (repeat 15 "-"))
-    (println )
-    (println )
-    (println (repeat 15 "_")))
+    (println "\n" (repeat 15 "-") "\n")
+    (println "\n" (repeat 15 "_") "\n"))
 
   (println "Total Correct:" ((:number-correct round-1)))
-  (println (repeat 15 "_"))
   (println "Percentage correct:" (str (int ((:percent-correct round-1))) "%"))
-  (println (repeat 15 "_"))
-  (println "Correct by Category:" (apply str (map #(str "\n" ((:number-correct-by-category round-1) %) " " %) categories)))
-  (println (repeat 15 "_"))
-  (println "% Correct by Category:" (apply str (map #(str "\n" (int ((:percent-correct-by-category round-1) %)) "% " %) categories))))
-(println (repeat 15 "_"))
+  (println "\n" (repeat 15 "_") "\n")
+  (println "Correct by Category:" (correct-by-category " " (:number-correct-by-category round-1)))
+  (println "\n" (repeat 15 "-") "\n")
+  (println "\n" (repeat 15 "_") "\n")
+  (println "% Correct by Category:" (correct-by-category "% " (:percent-correct-by-category round-1)))
+  (println "\n" (repeat 15 "-") "\n"))
