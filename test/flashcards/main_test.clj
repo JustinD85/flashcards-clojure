2452d1dbbcaa4b4270d7908f3eedaf97cef826f7
Justin Duncan <justin@baal-software.com>
2019-06-21 07:20:24 -0600

    Groups bindings together in test

--- test/flashcards/main_test.clj
+++ test/flashcards/main_test.clj
@@ -6,14 +6,10 @@
 (def card-2 (card "to be" "or not to be" :poetry))
 (def card-3 (card "what is the best temp?" "summer" :life))
 (def card-4 (card "what is the worst temp?" "winter" :life))
-
 (def cards [card-1 card-2 card-3 card-4])
-
 (def turn-1 (turn "nothing is" card-1))
 (def turn-2 (turn "something is" card-1))
-
 (def deck-1 (deck cards))
-
 (def round-1 (round deck-1))
 
 (deftest card-test
